cbe06012b5b197661df0e32d4274944a7b46fefa
gulu2016 <2412165411@qq.com>
2023-12-19 22:32:04 +0800

    add comment

--- demo02-servlet01/src/com/atguigu/servlet/UserServlet.java
+++ demo02-servlet01/src/com/atguigu/servlet/UserServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class UserServlet  extends HttpServlet {
+    //s67-3-1 if Tomcat create the object of this class, will call service function
+    //s67-3-2 take req as input, take resp as output
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 获取请求中的参数
