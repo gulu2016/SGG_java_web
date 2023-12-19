9620dae8479afd67ef3fa69459a9906d47c5b664
gulu2016 <2412165411@qq.com>
2023-12-19 23:23:08 +0800

    add comment

--- demo02-servlet01/src/com/atguigu/servlet/UserServlet.java
+++ demo02-servlet01/src/com/atguigu/servlet/UserServlet.java
@@ -14,6 +14,11 @@ public class UserServlet  extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 获取请求中的参数
         String username = req.getParameter("username");
+        //s68-1-1 first way to set content-Type in html response header
+        //resp.setHeader("Content-Type","text/html");
+        //s68-1-2 second way to set content-Type in html response header
+        //s68-1-3 Content-Type will tell client what type of content it is
+        resp.setContentType("text/html");
         if("atguigu".equals(username)){
             //通过响应对象响应信息
             resp.getWriter().write("NO");
